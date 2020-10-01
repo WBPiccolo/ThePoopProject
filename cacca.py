6c42a364c50536be15badb98b5212c3ba3216b4b
Francesco <piccolofrancesco39@gmail.com>
2020-10-01 15:50:57 +0200

    Tieni ti ho fixato tutto. Che poi i cvs nel 2020 zio

--- cacca.py
+++ cacca.py
@@ -1,4 +1,10 @@
 from datetime import date
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class OggettoCacca(object):
 	def __init__(self, number, mattina, pome, sera, notte, ratio, string):
@@ -16,209 +22,88 @@ class OggettoCacca(object):
 			return 1
 		else:
 			return 0			
-
 def diff_dates():
 	startDate = date(2019,12,8)
 	today = date.today()
 	return abs(today-startDate).days
 
 lista = []
-
-me = 0
-meM = 0
-meP = 0
-meS = 0
-meN = 0
-
-elia = 0
-eliaM = 0
-eliaP = 0
-eliaS = 0
-eliaN = 0
-
-gloria = 0
-gloriaM = 0
-gloriaP = 0
-gloriaS = 0
-gloriaN = 0
-
-veronica = 0
-veronicaM = 0
-veronicaP = 0
-veronicaS = 0
-veronicaN = 0
-
-boldro = 0
-boldroM = 0
-boldroP = 0
-boldroS = 0
-boldroN = 0
-
-step = 0
-stepM = 0
-stepP = 0
-stepS = 0
-stepN = 0
-
-giulia = 0
-giuliaM = 0
-giuliaP = 0
-giuliaS = 0
-giuliaN = 0
-
-favero = 0
-faveroM = 0
-faveroP = 0
-faveroS = 0
-faveroN = 0
-
-claudia = 0
-claudiaM = 0
-claudiaP = 0
-claudiaS = 0
-claudiaN = 0
-
-benny = 0
-bennyM = 0
-bennyP = 0
-bennyS = 0
-bennyN = 0
-
+maxCagate = 0
 differenza = diff_dates()
 
 print()
 print("Sono passati "+str(differenza)+" giorni dall'inizio (08/12/19)")
 print()
 
-f = open("C:\Workspaces\ThePoopProject\ThePoopProject\chat.txt", "r", encoding="utf8")
+
+merdaDizionario = dict()
+
+f = open("chat.txt", "r", encoding="utf8")
 for x in f:
 	orario = x[10:12]
-	if "Guglielmo: " in x:
-		benny+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			bennyM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			bennyP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			bennyS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			bennyN+=1
-	elif "Daniele: " in x:
-		me+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			meM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			meP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			meS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			meN+=1
-	elif "Mattia: " in x:
-		favero+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			faveroM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			faveroP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			faveroS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			faveroN+=1
-	elif ";: " in x:
-		gloria+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			gloriaM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			gloriaP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			gloriaS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			gloriaN+=1
-	elif "Nicolò B: " in x:
-		boldro+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			boldroM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			boldroP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			boldroS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			boldroN+=1
-	elif "Claudia: " in x:
-		claudia+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			claudiaM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			claudiaP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			claudiaS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			claudiaN+=1
-	elif "Giulia: " in x:
-		giulia+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			giuliaM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			giuliaP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			giuliaS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			giuliaN+=1
-	elif "Stefano: " in x:
-		step+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			stepM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			stepP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			stepS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			stepN+=1
-	elif "Veronica: " in x:
-		veronica+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			veronicaM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			veronicaP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			veronicaS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			veronicaN+=1
-	elif "Elia: " in x:
-		elia+=1
-		if (int(orario)>=6 and int(orario)<=12):
-			eliaM+=1
-		elif (int(orario)>=13 and int(orario)<=18):
-			eliaP+=1
-		elif (int(orario)>=19 and int(orario)<=23):
-			eliaS+=1
-		elif (int(orario)>=0 and int(orario)<=5):
-			eliaN+=1
+	messaggio = x[18:]
+	if(":" in messaggio):
+		mittente = messaggio[0:messaggio.index(':')]
+		if mittente in merdaDizionario:
+			if (int(orario)>=6 and int(orario)<=12):
+				merdaDizionario[mittente]['cagate']['mattino'] = merdaDizionario[mittente]['cagate']['mattino'] + 1
+			elif (int(orario)>=13 and int(orario)<=18):
+				merdaDizionario[mittente]['cagate']['pomeriggio'] = merdaDizionario[mittente]['cagate']['pomeriggio'] + 1
+			elif (int(orario)>=19 and int(orario)<=23):
+				merdaDizionario[mittente]['cagate']['sera'] = merdaDizionario[mittente]['cagate']['sera'] + 1
+			elif (int(orario)>=0 and int(orario)<=5):
+				merdaDizionario[mittente]['cagate']['notte'] = merdaDizionario[mittente]['cagate']['notte'] + 1
+		else:
+			#creo la voce
+			cagateElement = {}
+			if (int(orario)>=6 and int(orario)<=12):
+				cagateElement = {
+					'mattino': 1,
+					'pomeriggio': 0,
+					'sera': 0,
+					'notte':0,
+				}
+			elif (int(orario)>=13 and int(orario)<=18):
+				cagateElement = {
+					'mattino': 0,
+					'pomeriggio': 1,
+					'sera': 0,
+					'notte':0,
+				}
+			elif (int(orario)>=19 and int(orario)<=23):
+				cagateElement = {
+					'mattino': 0,
+					'pomeriggio': 0,
+					'sera': 1,
+					'notte':0,
+				}
+			elif (int(orario)>=0 and int(orario)<=5):
+				cagateElement = {
+					'mattino': 0,
+					'pomeriggio': 0,
+					'sera': 0,
+					'notte':1,
+				}
 
-bennyR = round((benny/differenza), 2)
-boldroR = round((boldro/differenza), 2)
-meR = round((me/differenza), 2)
-claudiaR = round((claudia/differenza), 2)
-giuliaR = round((giulia/differenza), 2)
-veronicaR = round((veronica/differenza), 2)
-faveroR = round((favero/differenza), 2)
-eliaR = round((elia/differenza), 2)
-stepR = round((step/differenza), 2)
-gloriaR = round((gloria/differenza), 2)
-
-lista.append(OggettoCacca(benny, bennyM, bennyP, bennyS, bennyN, bennyR, 'benny = '))
-lista.append(OggettoCacca(me, meM, meP, meS, meN, meR, 'io = '))
-lista.append(OggettoCacca(boldro, boldroM, boldroP, boldroS, boldroN, boldroR, 'boldro = '))
-lista.append(OggettoCacca(claudia, claudiaM, claudiaP, claudiaS, claudiaN, claudiaR, 'claudia = '))
-lista.append(OggettoCacca(giulia, giuliaM, giuliaP, giuliaS, giuliaN, giuliaR, 'giulia = '))
-lista.append(OggettoCacca(veronica, veronicaM, veronicaP, veronicaS, veronicaN, veronicaR, 'veronica = '))
-lista.append(OggettoCacca(favero, faveroM, faveroP, faveroS, faveroN, faveroR, 'favero = '))
-lista.append(OggettoCacca(elia, eliaM, eliaP, eliaS, eliaN, eliaR, 'elia = '))
-lista.append(OggettoCacca(step, stepM, stepP, stepS, stepN, stepR, 'step = '))
-lista.append(OggettoCacca(gloria, gloriaM, gloriaP, gloriaS, gloriaN, gloriaR, 'gloria = '))
-
+			dictionaryElement = {
+				'cagate': cagateElement,
+				'media':0,
+				'totale':0,
+			}
+			merdaDizionario[mittente] = dictionaryElement
+#calcolo le varie medie 
+for cagatore in merdaDizionario:
+	totaleCagate = merdaDizionario[cagatore]['cagate']['mattino'] + merdaDizionario[cagatore]['cagate']['pomeriggio'] + merdaDizionario[cagatore]['cagate']['sera'] + merdaDizionario[cagatore]['cagate']['notte']
+	merdaDizionario[cagatore]['totale'] = totaleCagate
+	merdaDizionario[cagatore]['media']= round((totaleCagate/differenza),2)
+	lista.append(OggettoCacca(merdaDizionario[cagatore]['totale'],
+		merdaDizionario[cagatore]['cagate']['mattino'],merdaDizionario[cagatore]['cagate']['pomeriggio'],merdaDizionario[cagatore]['cagate']['sera'], 
+		merdaDizionario[cagatore]['cagate']['notte'], merdaDizionario[cagatore]['media'],cagatore +' = '))
+	if totaleCagate > maxCagate:
+		maxCagate = totaleCagate
 lista.sort(reverse = True, key=lambda x: (x.number))  
 
-results = open("C:/Workspaces/ThePoopProject/ThePoopProject/results"+str(date.today())+".csv", "w")
+results = open("results"+str(date.today())+".csv", "w")
 outputString = ""
 outputCsv = "Nome, Totale, Ratio, Mattina, Pomeriggio, Sera, Notte\n"
 
@@ -235,5 +120,40 @@ for obj in lista:
 
 print(outputString)
 results.write(outputCsv)
-
 results.close()
+
+with open ('output.json', 'w', encoding='utf8') as output:
+	json.dump(merdaDizionario, output, ensure_ascii=False)
+
+
+N = 0
+mattinoBar = []
+pomeriggioBar = []
+seraBar = []
+notteBar = []
+names = []
+for cagatore in merdaDizionario:
+	N = N + 1
+	mattinoBar.append(int(merdaDizionario[cagatore]['cagate']['mattino']))
+	pomeriggioBar.append(int(merdaDizionario[cagatore]['cagate']['pomeriggio']))
+	seraBar.append(int(merdaDizionario[cagatore]['cagate']['sera']))
+	notteBar.append(int(merdaDizionario[cagatore]['cagate']['notte']))
+	names.append(cagatore)
+
+ind = np.arange(N)    # the x locations for the groups
+width = 0.35       # the width of the bars: can also be len(x) sequence
+
+
+
+
+p1 = plt.bar(ind, mattinoBar, width)
+p2 = plt.bar(ind, pomeriggioBar, width, bottom=mattinoBar)
+p3 = plt.bar(ind, seraBar, width, bottom=np.array(pomeriggioBar)+np.array(mattinoBar))
+p4 = plt.bar(ind, notteBar, width, bottom=np.array(pomeriggioBar)+np.array(mattinoBar)+np.array(seraBar))
+
+plt.ylabel('Cagate')
+plt.title('Che cagate')
+plt.xticks(ind, names)
+plt.yticks(np.arange(0, maxCagate, 50))
+plt.legend((p1[0], p2[0], p3[0], p4[0]), ('Mattino', 'Pomeriggio', 'Sera', 'Notte'))
+plt.show()
